66a734bd8981c8ae83b37a704b4859f8d17675b2
Gaurav Singh Jethuri <gauravsinghjethuri2002@gmail.com>
2023-12-08 22:33:00 +0530

    register controller fixes.

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,11 +4,11 @@ import {upload} from "../middlewares/multer.middleware.js"
 
 const router = Router();
 
-router.route("/register").get(
+router.route("/register").post(
     upload.fields(
         [
             {name: 'avatar', maxCount: 1},
-            {name: 'coverImage', maxCount: 1}
+            {name: 'coverImage', maxCount: 2}
         ]
     ),
     registerUser);
